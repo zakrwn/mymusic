514d46e23089c53d29625b2e6d3e0416682befa5
zakrwn <45009413+zakrwn@users.noreply.github.com>
2018-12-12 16:40:46 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,30 @@
-
 const Discord = require('discord.js');
-const client = new Discord.Client();
 
+const Util = require('discord.js');
 
+const getYoutubeID = require('get-youtube-id');
 
-const prefix = ".";
-client.on('message', async msg =>{
+const fetchVideoInfo = require('youtube-info');
+
+const YouTube = require('simple-youtube-api');
+
+const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
+
+const queue = new Map();
+
+const ytdl = require('ytdl-core');
+
+const fs = require('fs');
+
+const gif = require("gif-search");
+
+const client = new Discord.Client({disableEveryone: true});
+
+const prefix = "zz";
+/////////////////////////
+////////////////////////
+
+client.on('message', message => {
 	if (msg.author.bot) return undefined;
     if (!msg.content.startsWith(prefix)) return undefined;
     
