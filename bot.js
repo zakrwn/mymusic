00e76e760f79b154eda93f65b9c0c9a598933088
zakrwn <45009413+zakrwn@users.noreply.github.com>
2018-12-12 16:24:04 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -347,32 +347,6 @@ client.on('message', message => {
 });
 
 
-client.on('message', msg => {
-
-    if (msg.content == 'zzjoin') {
-        if (msg.member.voiceChannel) {
-
-     if (msg.member.voiceChannel.joinable) {
-         msg.member.voiceChannel.join().then(msg.react('white_check_mark'));
-     }
-    }
-}
-})
----------------------
-client.on('ready', () => { //code bot not leave room voice //Bot Is Online
-    client.channels.get("ايدي الروم").join(); //by : Toxic Codes
-    });client.on('message', msg => {
-
-    if (msg.content == 'zzjoin') {
-        if (msg.member.voiceChannel) {
-
-     if (msg.member.voiceChannel.joinable) {
-         msg.member.voiceChannel.join().then(msg.react('white_check_mark'));
-     }
-    }
-}
-})
-
 
 
 
