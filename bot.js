2ce188df080f42876ecbb28053ecea0f5eecf44e
zakrwn <45009413+zakrwn@users.noreply.github.com>
2018-12-12 22:55:50 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,30 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
 
-const prefix = "zz";
+const Util = require('discord.js');
+
+const getYoutubeID = require('get-youtube-id');
+
+const fetchVideoInfo = require('youtube-info');
+
+const YouTube = require('simple-youtube-api');
+
+const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
+
+const queue = new Map();
+
+const ytdl = require('ytdl-core');
+
+const fs = require('fs');
+
+const gif = require("gif-search");
+
+const client = new Discord.Client({disableEveryone: true});
+
+const prefix = "A";
 /////////////////////////
 ////////////////////////
 
-client.on('message', message => {
+client.on('message', async msg =>{
 	if (msg.author.bot) return undefined;
     if (!msg.content.startsWith(prefix)) return undefined;
     
@@ -293,10 +312,6 @@ function play(guild, song) {
 }
 
 
-
-
-
-
 client.on('message', message => {
     if (message.content === 'help') {
         let helpEmbed = new Discord.RichEmbed()
@@ -332,8 +347,11 @@ client.on('message', message => {
 
 
 
-const devs = ["295909228328255488","394949560327208960"]; //الايدي حقك
-const adminprefix = ["zz"]; // البرفكس
+
+
+
+const devs = ["412656333855260672","388279764818853890"]; //الايدي حقك
+const adminprefix = ["A"]; // البرفكس
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!devs.includes(message.author.id)) return;
